86838ccb46b71894236f4d0c171c79b33287c294
Roger <roger.alex.aguilar@gmail.com>
2020-10-04 12:53:35 -0700

    need fixed

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
+const playerChoosesPaper = document
+  .getElementById("paper")
+  .addEventListener("click", runGame);
+const playerChoosesScissor = document
+  .getElementById("scissor")
+  .addEventListener("click", runGame);
+const playerChoosesRock = document
+  .getElementById("rock")
+  .addEventListener("click", runGame);
+
+let playerChoice;
+let aiChoice;
+
+function runGame() {
+  playerChoice = event.target.getAttribute("alt");
+
+  playerPick(playerChoice);
+  aiChoosesRandom();
+  countdownToStartBattle();
+  playAgain();
+}
+
 const countdownToStartBattle = () => {
   const matchTimer = document.querySelector(".match-timer");
   let seconds = 4;
-  let timer = setInterval(() => {
+  setInterval(() => {
     seconds--;
     matchTimer.innerHTML = `Starting in... ${seconds}`;
     if (seconds < 0) {
@@ -14,64 +36,22 @@ const playAgain = () => {
   setTimeout(() => {
     let askPlayer = confirm("Want to Play Again?");
 
-    if (askPlayer === true) {
-      location.reload();
-    } else {
-      location.reload();
+    if (askPlayer) {
+      return location.reload();
     }
   }, 6000);
 };
 
-const playerChoosesPaper = document
-  .querySelector(".paper")
-  .addEventListener("click", (e) => {
-    const playersChoice = document.querySelector(".player-choice");
-    playersChoice.innerHTML = "You chose PAPER!";
+const playerPick = (choice) => {
+  playersChoice = document.querySelector(".player-choice");
+  playersChoice.innerHTML = `You chose ${choice}`;
 
-    const paperBackground = document.querySelector(".paper");
-    paperBackground.style.backgroundColor = "#1b6ca8";
+  // const choiceBackground = document.getElementById(`${choice}`);
+  // choiceBackground.style.backgroundColor = "#1b6ca8";
 
-    const aiAnnouncement = document.querySelector(".ai-announcement");
-    aiAnnouncement.style.visibility = "visible";
-
-    aiChoosesRandom();
-    countdownToStartBattle();
-    playAgain();
-  });
-
-const playerChoosesScissor = document
-  .querySelector(".scissor")
-  .addEventListener("click", (e) => {
-    const playersChoice = document.querySelector(".player-choice");
-    playersChoice.innerHTML = "You chose SCISSOR!";
-
-    const scissorBackground = document.querySelector(".scissor");
-    scissorBackground.style.backgroundColor = "#1b6ca8";
-
-    const aiAnnouncement = document.querySelector(".ai-announcement");
-    aiAnnouncement.style.visibility = "visible";
-
-    aiChoosesRandom();
-    countdownToStartBattle();
-    playAgain();
-  });
-
-const playerChoosesRock = document
-  .querySelector(".rock")
-  .addEventListener("click", (e) => {
-    const playersChoice = document.querySelector(".player-choice");
-    playersChoice.innerHTML = "You chose ROCK!";
-
-    const rockBackground = document.querySelector(".rock");
-    rockBackground.style.backgroundColor = "#1b6ca8";
-
-    const aiAnnouncement = document.querySelector(".ai-announcement");
-    aiAnnouncement.style.visibility = "visible";
-
-    aiChoosesRandom();
-    countdownToStartBattle();
-    playAgain();
-  });
+  const aiAnnouncement = document.querySelector(".ai-announcement");
+  aiAnnouncement.style.visibility = "visible";
+};
 
 const aiChoosesRandom = () => {
   const aiRock = document.querySelector(".ai-rock");
@@ -81,26 +61,29 @@ const aiChoosesRandom = () => {
   let randomNumber = Math.floor(Math.random() * 100);
   console.log(randomNumber);
 
-  if (0 <= randomNumber && randomNumber < 34) {
-    return setTimeout(() => {
+  setTimeout(() => {
+    if (0 <= randomNumber && randomNumber < 34) {
       aiScissor.style.display = "none";
       aiPaper.style.display = "none";
-    }, 4000);
-  }
+      aiChoice = "rock";
+    }
 
-  if (34 <= randomNumber && randomNumber < 67) {
-    return setTimeout(() => {
+    if (34 <= randomNumber && randomNumber < 67) {
       aiRock.style.display = "none";
       aiScissor.style.display = "none";
-    }, 4000);
-    return;
-  }
+      aiChoice = "paper";
+    }
 
-  if (67 <= randomNumber && randomNumber < 100) {
-    return setTimeout(() => {
+    if (67 <= randomNumber && randomNumber < 100) {
       aiRock.style.display = "none";
       aiPaper.style.display = "none";
-    }, 4000);
-    return;
-  }
+      aiChoice = "scissor";
+    }
+    gameResult();
+  }, 4000);
 };
+
+function gameResult() {
+  let computerChoice = document.querySelector(".ai-choice");
+  let winner = document.querySelector(".winner");
+}
